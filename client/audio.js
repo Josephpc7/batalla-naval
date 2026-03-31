ce148783d39948867d39ddfb44878faf26559584
analistasistemas <analistasistemas@laconcepcion.org>
2026-03-31 10:33:17 -0500

    fixing socket

--- client/audio.js
+++ client/audio.js
@@ -1,6 +1,14 @@
-const explosionSound = new Howl({ src: ['https://actions.google.com/sounds/v1/explosions/explosion.ogg'] });
-const waterSound = new Howl({ src: ['https://actions.google.com/sounds/v1/water/water_splash.ogg'] });
-const radarSound = new Howl({ src: ['https://actions.google.com/sounds/v1/alarms/beep_short.ogg'] });
+const explosionSound = new Howl({
+  src: ['https://cdn.pixabay.com/download/audio/2022/03/15/audio_115b9b3c1d.mp3']
+});
+
+const waterSound = new Howl({
+  src: ['https://cdn.pixabay.com/download/audio/2022/03/10/audio_273b8b4f9c.mp3']
+});
+
+const radarSound = new Howl({
+  src: ['https://cdn.pixabay.com/download/audio/2021/08/04/audio_c6ccf9a8b2.mp3']
+});
 
 function playExplosion() { explosionSound.play(); }
 function playWater() { waterSound.play(); }
